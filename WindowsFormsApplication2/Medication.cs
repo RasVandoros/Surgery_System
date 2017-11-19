5aae0e630001142430ec40e1bdc6a78ffce75990
Ras <Ras@DESKTOP-U62TEJ6>
2017-11-19 15:39:28 +0000

    Adding Extendability(Tests in DB, Staffmember and Medication classes)

---
+++ WindowsFormsApplication2/Medication.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class Medication
+    {
+        private string id;
+        private string name;
+        private string notes;
+
+
+        public string ID
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public string Notes
+        {
+            get
+            {
+                return this.notes;
+            }
+        }
+
+        /// <summary>
+        /// This class was not utilized for the creation of the project. It was created to provide extendability later on.
+        /// </summary>
+        /// <param name="data"></param>
+        public Medication(DataSet data)
+        {
+            this.id = data.Tables[0].Rows[0][0].ToString().Trim(' ');
+            this.name = data.Tables[0].Rows[0][1].ToString().Trim(' ');
+            this.notes = data.Tables[0].Rows[0][2].ToString().Trim(' ');
+
+
+            Logger.Instance.WriteLog(Logger.Type.Flow, new Message("Med object instantiated"), UIManager.Instance.ID.ToString());
+
+        }
+    }
+}
---
+++ WindowsFormsApplication2/StaffMember.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2
+{
+    class StaffMember
+    {
+        private int id;
+        private string name;
+        private Job job;
+
+
+        public int Id
+        {
+            get
+            {
+                return this.id;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+        }
+
+        public Job Job
+        {
+            get
+            {
+                return this.job;
+            }
+        }
+
+        /// <summary>
+        /// This whole class was design to provide extendability to the software. At the moment it is not utilized within the code.
+        /// </summary>
+        /// <param name="data"></param>
+        public StaffMember(DataSet data)
+        {
+            this.id = Int32.Parse(data.Tables[0].Rows[0][0].ToString().Trim(' '));
+            this.name = data.Tables[0].Rows[0][1].ToString().Trim(' ');
+            Dictionary<string, Job> dictionary = new Dictionary<string, Job>();
+            dictionary.Add("Nurse", Job.Nurse);
+            dictionary.Add("Doctor", Job.Doctor);
+            string test = data.Tables[0].Rows[0][3].ToString().Trim(' ');
+            this.job = dictionary[test];
+
+        }
+    }
+}
